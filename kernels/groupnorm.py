8d2d383202e4fd573ba5acd0b7e63be8a9e2577e
AandD <74431077+arnavdantuluri@users.noreply.github.com>
2023-09-19 18:45:34 -0400

    Add files via upload

---
+++ kernels/groupnorm.py
@@ -0,0 +1,109 @@
+# Copyright 2023 ⓒ Kakao Brain Corp.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import triton
+import triton.language as tl
+
+@triton.jit
+def group_norm(
+    output_ptr: tl.tensor,
+    input_ptr: tl.tensor,
+    rstd_ptr: tl.tensor,
+    mean_ptr: tl.tensor,
+    y_size: tl.int32,
+    x_size: tl.int32,
+    num_groups: tl.int32,
+    weight_ptr: tl.tensor,
+    bias_ptr: tl.tensor,
+    eps: tl.float32,
+    dtype: tl.constexpr,
+    y_block_size: tl.constexpr,
+    x_block_size: tl.constexpr,
+):
+    group_size = y_size // num_groups
+    pid = tl.program_id(0)
+    batch = pid // num_groups
+    group = pid % num_groups
+    batch_offset = batch * y_size * x_size
+    num_elements = group_size * x_size
+    group_offset = batch_offset + group * num_elements
+    output_block_ptr = tl.make_block_ptr(
+        output_ptr + group_offset,
+        shape=(group_size, x_size),
+        strides=(x_size, 1),
+        offsets=(0, 0),
+        block_shape=(y_block_size, x_block_size),
+        order=(1, 0),
+    )
+    input_block_ptr = tl.make_block_ptr(
+        input_ptr + group_offset,
+        shape=(group_size, x_size),
+        strides=(x_size, 1),
+        offsets=(0, 0),
+        block_shape=(y_block_size, x_block_size),
+        order=(1, 0),
+    )
+    rstd_block_ptr = tl.make_block_ptr(
+        rstd_ptr + batch * num_groups,
+        shape=(group_size,),
+        strides=(1,),
+        offsets=(group,),
+        block_shape=(1,),
+        order=(0,),
+    )
+    mean_block_ptr = tl.make_block_ptr(
+        mean_ptr + batch * num_groups,
+        shape=(group_size,),
+        strides=(1,),
+        offsets=(group,),
+        block_shape=(1,),
+        order=(0,),
+    )
+    input = tl.load(input_block_ptr, boundary_check=(0, 1), padding_option="zero")
+    mean = tl.sum(tl.view(input / num_elements, (1, y_block_size * x_block_size)), 1)
+    y_condition = tl.arange(0, y_block_size) < group_size
+    x_condition = tl.arange(0, x_block_size) < x_size
+    condition = y_condition[:, None] & x_condition[None, :]
+    centered_mean = tl.where(condition, input - mean, 0)
+    var = tl.sum(tl.view(centered_mean * centered_mean / num_elements, (1, y_block_size * x_block_size)), 1)
+    rstd = tl.math.rsqrt(var + eps)
+    output = centered_mean * rstd
+
+    if weight_ptr is not None:
+        weight_block_ptr = tl.make_block_ptr(
+            weight_ptr,
+            shape=(y_size, 1),
+            strides=(1, y_size),
+            offsets=(group * group_size, 0),
+            block_shape=(y_block_size, 1),
+            order=(0, 1),
+        )
+        weight = tl.load(weight_block_ptr, boundary_check=(0,))
+        output *= weight
+
+    if bias_ptr is not None:
+        bias_block_ptr = tl.make_block_ptr(
+            bias_ptr,
+            shape=(y_size, 1),
+            strides=(1, y_size),
+            offsets=(group * group_size, 0),
+            block_shape=(y_block_size, 1),
+            order=(0, 1),
+        )
+        bias = tl.load(bias_block_ptr, boundary_check=(0,))
+        output += bias
+
+    tl.store(output_block_ptr, output.to(dtype), boundary_check=(0, 1))
+    tl.store(rstd_block_ptr, rstd.to(dtype))
+    tl.store(mean_block_ptr, mean.to(dtype))
---
+++ optimizer/tests/test_graph_replacement.py
@@ -0,0 +1,26 @@
+# Needs to be done for every forward function in sdxl_original_unet
+import torch
+from torch.fx import symbolic_trace, subgraph_rewriter
+
+class M(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x, w1, w2):
+        m1 = torch.cat([w1, w2]).sum()
+        m2 = torch.cat([w1, w2]).sum()
+        return x + torch.max(m1) + torch.max(m2)
+
+def pattern(x, w1, w2):
+    m1 = torch.cat([w1, w2]).sum()
+    m2 = torch.cat([w1, w2]).sum()
+    return x + torch.max(m1) + torch.max(m2)
+
+def replacement(x, w1, w2):
+    return torch.stack([w1, w2])
+
+traced_module = symbolic_trace(M())
+
+print(traced_module.code)
+subgraph_rewriter.replace_pattern(traced_module, pattern, replacement)
+print(traced_module.code)
